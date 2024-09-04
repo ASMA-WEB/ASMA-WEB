71f378031ae04e616ed3580101b3561740b6dd13
Muzammil Ismail <muzammilismail8@gmail.com>
2024-09-04 13:47:27 +0500

    Reformating

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,14 +1,13 @@
+import { lazy } from "react";
+import { useRoutes } from "react-router-dom";
 
-import { lazy } from 'react';
-import { useRoutes } from 'react-router-dom';
-
-const Signup = lazy(() => import('pages/SignUp'));
-const Login =lazy(()=> import ('pages/Login'));
+const Signup = lazy(() => import("pages/SignUp"));
+const Login = lazy(() => import("pages/Login"));
 
 const useRoutesHook = () => {
   let element = useRoutes([
-    { path: '/', element: <Signup /> },
-    { path: '/login', element: <Login /> },
+    { path: "/", element: <Signup /> },
+    { path: "/login", element: <Login /> },
   ]);
   return element;
 };
