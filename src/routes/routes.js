a35666c14712aae728c9de727389725068ff3ca2
Asma <56886888+ASMA-WEB@users.noreply.github.com>
2024-09-04 15:41:45 +0500

    Revert "Reformating"

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,13 +1,14 @@
-import { lazy } from "react";
-import { useRoutes } from "react-router-dom";
 
-const Signup = lazy(() => import("pages/SignUp"));
-const Login = lazy(() => import("pages/Login"));
+import { lazy } from 'react';
+import { useRoutes } from 'react-router-dom';
+
+const Signup = lazy(() => import('pages/SignUp'));
+const Login =lazy(()=> import ('pages/Login'));
 
 const useRoutesHook = () => {
   let element = useRoutes([
-    { path: "/", element: <Signup /> },
-    { path: "/login", element: <Login /> },
+    { path: '/', element: <Signup /> },
+    { path: '/login', element: <Login /> },
   ]);
   return element;
 };
